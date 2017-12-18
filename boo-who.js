b2d3ee96d7276ec11ca68376451723c0dbb16e34
jackhowa <jackhowaa@gmail.com>
2017-12-18 07:57:23 -0600

    Add evaluator using typeof for bool

--- boo-who.js
+++ boo-who.js
@@ -1,9 +1,12 @@
-function booWho(bool) {
-  // What is the new fad diet for ghost developers? The Boolean.
-  return bool === true;
+function booWho(input) {
+  if (typeof input === "boolean") {
+    // test if any bool object
+    return true;
+  }
+  return input === true; // returns true based on evaluated value
 }
 
 booWho(null);
 // booWho(true) should return true.
-// booWho(false) should return true. nope
+// booWho(false) should return true. yep in line 3 
 // booWho([1, 2, 3]) should return false.
