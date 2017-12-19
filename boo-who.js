750a8ac4553765657e263186a5beedaa73841e8c
jackhowa <jackhowaa@gmail.com>
2017-12-18 19:22:54 -0600

    Refactor to make one-liner using typeof

--- boo-who.js
+++ boo-who.js
@@ -1,12 +1,5 @@
 function booWho(input) {
-  if (typeof input === "boolean") {
-    // test if any bool object
-    return true;
-  }
-  return input === true; // returns true based on evaluated value
+  return typeof input === "boolean"; // refactor to take out unnec after if check
 }
 
 booWho(null);
-// booWho(true) should return true.
-// booWho(false) should return true. yep in line 3 
-// booWho([1, 2, 3]) should return false.
